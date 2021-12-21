cb6ae27bb435f0b27c239a6a1e3810647d147937
Kate Neuse <kneu4581@gmail.com>
2021-12-20 20:42:01 -0600

    initial commit

---
+++ javascript.js
@@ -0,0 +1,17 @@
+var questions = [
+{
+    title: "Commonly used data types DO NOT include: ",
+    choices: ["strings","booleans", "alerts", "numbers"],
+    answer: "alerts"
+},
+
+
+];
+
+var score = 0;
+var questions = 0;
+
+var container = document.getElementById("container");
+var intro = document.getElementById("container");
+var questions = document.getElementById("container");
+var start = document.getElementById("container");
